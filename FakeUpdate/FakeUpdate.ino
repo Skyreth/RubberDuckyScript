bedfb3040a63fc57f167c630dec6aafc605ab46a
Skyreth <63458729+Skyreth@users.noreply.github.com>
2021-11-19 00:16:25 +0100

    Add files via upload

---
+++ FakeUpdate/FakeUpdate.ino
@@ -0,0 +1,34 @@
+#include "Keyboard.h"
+
+void setup() {
+  Keyboard.begin();
+  delay(1000);
+  runscript();
+  Keyboard.end();
+}
+
+void processKey(int key) {
+  Keyboard.press(key);
+  delay(500);
+  Keyboard.release(key);
+}
+
+void runscript() {
+  Keyboard.press(KEY_LEFT_GUI);
+  delay(500);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  Keyboard.print("cmd");
+  processKey(KEY_INSERT);
+
+  Keyboard.print("start msedge https://fakeupdate.net/win10ue/");
+  processKey(KEY_INSERT);
+
+  processKey(KEY_F10);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
